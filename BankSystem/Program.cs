ff22507328b06d64b02ccd5b0b649a3ff904968e
ygreksh <ygreksh@gmail.com>
2021-08-21 21:02:21 +0300

    Extract GetData to file

--- BankSystem/Program.cs
+++ BankSystem/Program.cs
@@ -11,30 +11,11 @@ namespace BankSystem
         {
             BankService bankService = new BankService();
 
-            //Client Ivanov = new Client() { Name = "Иванов", Age = 31, PassportNumber = "iv-11111111" };
-            var client1 = TestDataService.TestClientGenerate();
-            Client Petrov = new Client(){Name = "Петров", Age = 32, PassportNumber = "pe-22222222"};
-            Client Sidorov = new Client(){Name = "Сидоров", Age = 33, PassportNumber = "si-33333333"};
-            bankService.listOfClients.Add(client1);
-            bankService.listOfClients.Add(Petrov);
-            bankService.listOfClients.Add(Sidorov);
-            
-            Dollar dollar = new Dollar() { CurrencyName = "dollar", rate = 1 };
-            Ruble ruble = new Ruble() { CurrencyName = "ruble", rate = 77 };
-            Leu leu = new Leu() { CurrencyName = "leu", rate = 18 };
-            Hryvnia hryvnia = new Hryvnia() { CurrencyName = "hryvnia", rate = 27 };
-
+            Client client1 = TestDataService.TestClientGenerate();
             Account account1 = TestDataService.TestAccountGenerate();
-            Account ivanovaccount2 = new Account() { currency = ruble, value = 1000 };
-            Account petrovaccount1 = new Account() { currency = ruble, value = 2000 };
-            Account petrovaccount2 = new Account() { currency = leu, value = 2000 };
-            Account petrovaccount3 = new Account() { currency = hryvnia, value = 20000 };
-            Account sidorovaccount1 = new Account() { currency = ruble, value = 3000 }; 
             
-            ExtractData.GetData(client1);
-            ExtractData.GetData(dollar);
-            ExtractData.GetData(account1);
-            //ExtractData.GetData(bankService.listOfClients);
+            ExtractData.GetData(client1,"client.txt");
+            ExtractData.GetData(account1,"account1.txt");
         }
         
         
